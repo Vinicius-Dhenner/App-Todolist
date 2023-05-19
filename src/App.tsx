f78872cab75f2764a966f61b76dac007b53370a2
Vinicius-Dhenner <Vinicius.dhenner2003@gmail.com>
2023-05-18 21:05:13 -0400

    adcionando-tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-
-import { useTheme, TextField, Grid, AppBar, Toolbar, Typography, Container, Button, colors, Card, CardContent, Badge, Checkbox } from '@mui/material';
-import { ClipboardText, PlusCircle, Rocket } from "@phosphor-icons/react";
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import {
+  useTheme,
+  TextField,
+  Grid,
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Button,
+  colors,
+  Card,
+  CardContent,
+  Badge,
+  Checkbox
+} from '@mui/material';
+import { Rocket, PlusCircle } from "@phosphor-icons/react";
 import { styled } from '@mui/material/styles';
-import { ChangeEvent, useEffect, useState } from 'react';
 import { api, getAll, save } from './service/api';
 import { Task } from './types';
 import { CardTarefa } from './CardTarefa/inde';
-
-
 
 const CssTextField = styled(TextField)({
   '& label': {
@@ -16,7 +27,6 @@ const CssTextField = styled(TextField)({
   '& label.Mui-focused': {
     color: '#cfcfcf',
   },
-
   '& .MuiOutlinedInput-root': {
     '& fieldset': {
       borderColor: 'black',
@@ -31,28 +41,26 @@ const CssTextField = styled(TextField)({
   },
 });
 
-
 function App() {
-  const theme = useTheme()
-
-
+  const theme = useTheme();
   const [tasksi, setTasksi] = useState<string>('');
-  const [tasks, setTasks] = useState<Task[]>([])
-  const [teste, setTeste] = useState<any>()
-  const [criar, setCriar] = useState<string>('');
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [teste, setTeste] = useState<any>();
 
   useEffect(() => {
     pegaDados();
   }, [teste]);
 
   const saveNoBanco = () => {
-    const tsk = {
+    const newTask: Task = {
       description: tasksi,
       done: false
-    }
-    setTeste(tsk)
-    save(tsk);
-  }
+    };
+    setTasks(prevTasks => [...prevTasks, newTask]);
+    save(newTask)
+      .then(() => setTeste(newTask))
+      .catch(error => console.log(error));
+  };
 
   const pegaDados = async () => {
     try {
@@ -61,13 +69,10 @@ function App() {
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-
-
-  console.log(tasks)
+  console.log(tasks);
   return (
-
     <>
       <AppBar position='static'>
         <Toolbar sx={{
@@ -77,11 +82,9 @@ function App() {
           justifyContent: 'center',
           alignItems: 'center',
           '@media all': {
-
             minHeight: 200,
           }
         }}>
-
           <Typography variant="h5" component="h6" sx={{
             display: 'flex',
             alignItems: 'center',
@@ -92,7 +95,7 @@ function App() {
         </Toolbar>
       </AppBar>
       <main>
-        <Container sx={{ position: 'relative', }}>
+        <Container sx={{ position: 'relative' }}>
           <Grid container spacing={2} sx={{
             position: 'absolute',
             top: '-26px'
@@ -100,7 +103,6 @@ function App() {
             <Grid item xl={10} xs={12}>
               <CssTextField name='tasks' variant='outlined' label='Task' onChange={(e: ChangeEvent<HTMLInputElement>) => setTasksi(e.target.value)} placeholder='Adicione uma Nova tarefa' fullWidth sx={{
                 backgroundColor: colors.grey[900],
-
               }} />
             </Grid>
             <Grid item xl={2} xs={12} >
@@ -111,46 +113,43 @@ function App() {
                   backgroundColor: '#2b7aab',
                   transition: '.4s',
                 }
-              }}>Criar <PlusCircle size={30} /></Button>
+              }}>Criar<PlusCircle size={30} /></Button>
             </Grid>
           </Grid>
         </Container>
-
-
       </main>
       <Container sx={{ paddingTop: '100px' }}>
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', margin: '0px 18px 30px 0px' }}>
           <div>
             <div style={{ display: 'flex', alignItems: 'center', gap: '30px' }}>
-              <span style={{ color: '#52b2ec', fontSize: '20px', fontWeight: '600' }} >
+              <span style={{ color: '#52b2ec', fontSize: '20px', fontWeight: '600' }}>
                 Tarefas criadas
               </span>
               <Badge sx={{
                 '& .MuiBadge-badge': {
-                  backgroundColor: '#383838', 
+                  backgroundColor: '#383838',
                   textAlign: 'center',
                   padding: '10px'
                 },
-
-              }} >  </Badge>
+              }} >{tasks.length}</Badge>
             </div>
           </div>
           <div>
             <div style={{ display: 'flex', alignItems: 'center', gap: '30px' }}>
               <span style={{ color: '#5e60ce', fontSize: '20px', fontWeight: '600' }}>
-                Concluidas
+                Concluídas
               </span>
               <Badge sx={{
                 '& .MuiBadge-badge': {
-                  backgroundColor: '#383838', 
+                  backgroundColor: '#383838',
                   textAlign: 'center',
                   padding: '10px'
                 }
-              }} >  </Badge>
+              }} >0</Badge>
             </div>
           </div>
         </div>
-        <Grid container spacing={theme.spacing(1)} >
+        <Grid container spacing={theme.spacing(1)}>
           <Grid item xl={12} xs={12}>
             <Card sx={{
               minWidth: 275,
@@ -167,21 +166,16 @@ function App() {
                 height: '100%',
                 backgroundColor: '#121212'
               }}>
-
-
-
-                {tasks.map(dados => (
-
-                  <CardTarefa texto={dados.description} />
+                {tasks.map((task, index) => (
+                  <CardTarefa key={index} texto={task.description} />
                 ))}
-
               </CardContent>
             </Card>
           </Grid>
         </Grid>
-      </Container >
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
